f91e47e24b8e65283c9ac1b5a2fa3e6a107089b6
Alexey Petrov <a.petrov@thinglink.com>
2017-02-27 15:20:26 +0300

    Initial Commit

---
+++ Tetma_projTests/Tetma_projTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tetma_projTests.swift
+//  Tetma_projTests
+//
+//  Created by Alexey Petrov on 2/27/17.
+//  Copyright © 2017 Alexey Petrov. All rights reserved.
+//
+
+import XCTest
+@testable import Tetma_proj
+
+class Tetma_projTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
